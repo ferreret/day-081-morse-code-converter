3b8254c48bbdf8d365744497eb456bf7e89e32a1
Nicolás Barceló <ferreret@gmail.com>
2021-05-26 14:53:26 +0200

    Finished project

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("hola")
+from morse_code import translator
+
+initial_phrase = input("Introduce the phrase to convert:\n")
+
+words = initial_phrase.upper().split(' ')
+
+morse_code_result = []
+
+# Loop for every word I have in the phrase
+for word in words:
+    morse_word = [translator[letter] if letter in translator else letter for letter in word ]
+    morse_code_result.append(morse_word)
+
+print(morse_code_result)
